15e1099456b55e3d450132430c019248c532d3ba
Damiano <ghgg585@gmail.com>
2024-03-05 14:04:27 +0100

    Feat:add input js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //input km viaggio + età passeggero
+const travelLength = parseInt(prompt('Quanto è lungo il viaggio in km'));
+console.log(travelLength);
+const age = parseInt(prompt('Quanti anni hai?'));
+console.log(age);
 // varbiabili tasso di sconto  e definizione costo per km
 // applicazione sconto selezionato dalla fascia di età
 //output utente
